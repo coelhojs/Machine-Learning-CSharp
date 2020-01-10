fc755dd1055e40ae0986494528152acbd6d53d55
coelhojs <ccoelho.me@gmail.com>
2020-01-09 22:33:59 -0300

    Inicializando projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Machine_Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var detection = new ObjectDetection();
+            detection.Run();
+        }
+    }
+}
