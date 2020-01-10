212e1c1243940c59fde7094ae39d41f1a159b3bd
coelhojs <ccoelho.me@gmail.com>
2020-01-10 01:14:14 -0300

    Projeto inicializado

---
+++ Object Detection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Machine_Learning;
+
+namespace Object_Detection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var detection = new ObjectDetection();
+            detection.Run();
+        }
+    }
+}
